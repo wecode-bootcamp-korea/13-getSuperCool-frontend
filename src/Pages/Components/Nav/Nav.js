b569f22ee34730362330bd1fe3da53571d279c8a
msjulyone <msjulyone@gmail.com>
2020-10-22 16:22:38 +0900

    Nav component 분리

--- src/Pages/Components/Nav/Nav.js
+++ src/Pages/Components/Nav/Nav.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
+import Logo from "./Logo";
+import NavCenter from "./NavCenter"
+import NavRight from "./NavRight"
 import "./Nav.scss";
-import Logo from "./logo.svg";
 
 class Nav extends Component {
   render() {
     return (
       <div className="Nav">
-        <img src={Logo} alt="logo" />
-        <div className="NavCenter">
-          <p>SHOP</p>
-          <p>ABOUT</p>
-        </div>
-        <div className="NavRight">
-          <p>EUROPE</p>
-          <p>CART</p>
-          <div></div>
-          <div></div>
-        </div>
+        <Logo />
+        <NavCenter />
+        <NavRight />
       </div>
     );
   }
---
+++ src/Pages/Components/Nav/NavCenter.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NavCenter extends Component {
+  render() {
+    return (
+      <div className="NavCenter">
+        <span>SHOP</span>
+        <span>ABOUT</span>
+      </div>
+    );
+  }
+}
+
+export default NavCenter;
