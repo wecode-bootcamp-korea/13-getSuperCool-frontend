d9e8e26f922b0556b22ac92a94e90dfdb1740009
msjulyone <msjulyone@gmail.com>
2020-10-25 15:00:16 +0900

    Footer Layout

--- src/Pages/Components/Footer.js
+++ src/Pages/Components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
         </div>
         <div className="section2">
           <div className="detail">
-            <div>
+            <div className="subscribe">
               <p>SUBSCRIBE</p>
               <p>TO THE</p>
               <p>NEWSLETTER</p>
@@ -19,12 +19,21 @@ class Footer extends Component {
             <div className="help">
               <div>
                 <p>HELP</p>
+                <p>FAQ</p>
+                <p>Contact Us</p>
+                <p>Track Your Order</p>
+                <p>Return Your Order</p>
               </div>
               <div>
                 <p>LEGAL INFO</p>
+                <p>Privacy Policy</p>
+                <p>Terms & Conditions</p>
+                <p>Cookie Policy</p>
               </div>
               <div>
                 <p>FOLLOW US</p>
+                <p>Instagram</p>
+                <p>Facebook</p>
               </div>
             </div>
           </div>
