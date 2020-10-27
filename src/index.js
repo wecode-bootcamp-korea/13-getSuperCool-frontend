052ebf3444398401b9eab5d9fc1be3348fd88393
msjulyone <msjulyone@gmail.com>
2020-10-27 19:37:36 +0900

    merge main

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import { ParallaxProvider } from "react-scroll-parallax";
 
 import "./index.css";
-<<<<<<< HEAD
-import Main from "./Pages/Main/Main";
-
-ReactDOM.render(<Main />, document.getElementById("root"));
-=======
+//import Main from "./Pages/Main/Main";
 import "./Styles/common.scss";
 import "./Styles/reset.scss";
 
-
 ReactDOM.render(
   <ParallaxProvider>
-    <ProductDetails/>
+    <ProductDetails />
   </ParallaxProvider>,
   document.getElementById("root")
 );
->>>>>>> main
