acd84fbf3a414028e41799cda417dc90efb6b294
msjulyone <msjulyone@gmail.com>
2020-10-28 19:31:34 +0900

    main

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -9,8 +9,7 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
-      products: [],
-      
+      products: []
     };
   }
 
@@ -32,9 +31,7 @@ class Main extends Component {
       });
   }
 
-  coupon = () => {
-    
-  }
+  coupon = () => {};
 
   render() {
     const settings = {
@@ -57,15 +54,15 @@ class Main extends Component {
           <img
             id="first"
             src="https://images.ctfassets.net/vnxry7jc7f2k/3SBQaVgfnCvM6ALvpyYt2h/6dba0f1d03de947b436b8aa57c7ead30/slider-01.jpg?w=1800&h=1013&q=80&fm=webp"
-          ></img>
+          />
           <img
             id="second"
             src="https://images.ctfassets.net/vnxry7jc7f2k/1rJNXGIpDwniKyhAlBnRVM/9cfc8df2da07ebf13cb3229842820ab8/slider_applicazione.jpg?w=1920&h=1080&q=80&fm=webp"
-          ></img>
+          />
           <img
             id="third"
             src="https://images.ctfassets.net/vnxry7jc7f2k/5RpJJJgujK6rFOlN0ljzi7/06352b48f4acafa52500299cb6475299/leggera_SLIDER_MOOD.jpg?w=1920&h=1080&q=80&fm=webp"
-          ></img>
+          />
         </Slider>
         <div className="shop">
           <p>SHOP NOW</p>
